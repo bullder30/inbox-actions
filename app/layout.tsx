3275427b53860581be224f6c6e90db92f1db43a5
bullder30 <bullder30@users.noreply.github.com>
2026-01-28 13:52:50 +0100

    fix: new google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
 
-import { fontGeist, fontHeading, fontSans, fontUrban } from "@/assets/fonts";
-import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
-
 import { cn, constructMetadata } from "@/lib/utils";
-import { Toaster } from "@/components/ui/sonner";
+import { fontGeist, fontHeading, fontSans, fontUrban } from "@/assets/fonts";
+
 import { Analytics } from "@/components/analytics";
 import ModalProvider from "@/components/modals/providers";
+import { SessionProvider } from "next-auth/react";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -19,7 +19,9 @@ export const metadata = constructMetadata();
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <meta name="google-site-verification" content="UQ0xMNhEgPBsQzoSXW_t1f2Uhro5ky6qZ3ZWtI8vyok" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
